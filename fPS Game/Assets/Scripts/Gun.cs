70ded14d5f10cf9fdea64d9ca4db947294a269c1
wingRexx98 <47964600+wingRexx98@users.noreply.github.com>
2019-07-02 13:01:03 +0700

    Basic gun shooting with raycast
    Basic gun shooting with raycast, still needs work on the particle effects

---
+++ fPS Game/Assets/Scripts/Gun.cs
@@ -0,0 +1,53 @@
+﻿
+using UnityEngine;
+
+public class Gun : MonoBehaviour
+{
+    public float damage;
+    public float distance;
+    public float impactForce;
+    public float fireRate;
+
+    public Camera fpsCam;
+
+    public ParticleSystem shot;
+
+    public GameObject impactEffect;
+
+    private float nextToFire;
+    
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1") && Time.time >= nextToFire)
+        {
+            nextToFire = Time.time + 1f/ fireRate;
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        shot.Play();
+        RaycastHit hitInfo;
+        if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hitInfo, distance))
+        {
+            Debug.Log(hitInfo.transform.name);
+
+            target tar = hitInfo.transform.GetComponent<target>();
+            if(tar != null)
+            {
+                tar.TakeDamage(damage);
+            }
+
+            if(hitInfo.rigidbody != null)
+            {
+                hitInfo.rigidbody.AddForce(-hitInfo.normal * impactForce);
+            }
+
+            GameObject impactGO = Instantiate(impactEffect, hitInfo.point, Quaternion.LookRotation(hitInfo.normal));
+            Destroy(impactGO, 2f);
+        }
+    }
+}
---
+++ fPS Game/Assets/Scripts/target.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class target : MonoBehaviour
+{
+    public float health = 50f;
+
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+        if(health <= 0f)
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Destroy(gameObject);
+    }
+}
