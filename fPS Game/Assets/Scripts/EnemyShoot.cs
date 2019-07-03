e0612524ae47a2f6c85165fb4468983228fe5a5f
wingRexx98 <47964600+wingRexx98@users.noreply.github.com>
2019-07-03 12:18:50 +0700

    enemy shooting with prefab
    Starting the work on making enemy shoot player using prefab

---
+++ fPS Game/Assets/Scripts/EnemyShoot.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyShoot : MonoBehaviour
+{
+    public float fireRate;
+
+    public GameObject bulletPrefab;
+
+    public Transform firePoint;
+
+    private float nextToFire;
+
+    public void Shoot()
+    {
+        if (Time.time >= nextToFire)
+        {
+            nextToFire = Time.time + 1f / fireRate;
+            Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
+        }
+        
+    }
+}
---
+++ fPS Game/Assets/Scripts/Observer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Observer : MonoBehaviour
+{
+    public Transform player;// in stead gameobject, transform is use to determine whether or not the player is in view or whther the gargoyle can see the player
+    bool isInRange;
+    public float fireRate = 15f;
+    public EnemyShoot shoot;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.transform == player)
+        {
+            isInRange = true;
+            Debug.Log("Player in range");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.transform == player)
+        {
+            isInRange = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isInRange)
+        {
+            shoot.Shoot();
+        }
+    }
+}
